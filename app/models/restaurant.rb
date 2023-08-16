c08e5e305238fe5660da1ae19af1ab0e54aa7d51
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-16 10:04:16 +0100

    reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant < ApplicationRecord
   validates :name, presence: true
+
+  has_many :reviews, dependent: :destroy
 end
+
+# List all the reviews to that @restaurant
+# get all the review instances that has restuarant_id of @restaurant.id
+# Review.where(restuarant_id: @restaurant.id)
